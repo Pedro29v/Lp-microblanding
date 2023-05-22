a7daacc4150d5f9674bf90df011a2e41a5782298
Pedro <pedrocastillo24.pc@gmail.com>
2023-05-22 20:43:37 -0300

    create title for Service section

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,5 +1,14 @@
 function Service() {
-  return <div className="w-screen h-[100vh] "></div>;
+  return (
+    <div className="w-screen h-[100vh] bg-green-300">
+      <div
+        className="w-[100%] p-4 
+      text-center text-[1.4em] text-pink-500 "
+      >
+        <h2>Nossos serviços</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Service;
