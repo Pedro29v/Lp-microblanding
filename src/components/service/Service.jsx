009d93d79eb62058ffcb411d7155e4885e5f3ba9
Pedro <pedrocastillo24.pc@gmail.com>
2023-05-23 20:21:34 -0300

    create card microshading mobile

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,4 +1,5 @@
 import microblading from "../../assets/microblading2.png";
+import microshading from "../../assets/microshading.jpg";
 
 function Service() {
   return (
@@ -10,22 +11,44 @@ function Service() {
         <h2>Nossos serviços</h2>
       </div>
 
-      <div
-        className="w-[80%] h-[28rem] m-auto bg-white rounded-sm
+      <div>
+        <div
+          className="w-[80%] h-[28rem] m-auto bg-white rounded-sm
       text-center p-2 "
-      >
-        <h3 className="text-fontColor">Microblading</h3>
+        >
+          <h3 className="text-fontColor">Microblading</h3>
 
-        <p className="mt-4 text-sm ">
-          O microblading é um tipo de design de sobrancelha onde são desenhados
-          fios bem finos que se misturam aos fios naturais.
-        </p>
+          <p className="mt-4 text-sm ">
+            O microblading é um tipo de design de sobrancelha onde são
+            desenhados fios bem finos que se misturam aos fios naturais.
+          </p>
+
+          <div
+            className="w-[70%] h-[18rem] mt-8 m-auto rounded-sm 
+          bg-cover bg-no-repeat"
+            style={{ backgroundImage: `url(${microblading})` }}
+          ></div>
+        </div>
 
         <div
-          className="w-[70%] h-[18rem] mt-8 m-auto rounded-sm 
+          className="w-[80%] h-[28rem] m-auto mt-10 bg-white rounded-sm
+      text-center p-2 "
+        >
+          <h3 className="text-fontColor">microshading</h3>
+
+          <p className="mt-4 text-sm ">
+            Microshading é uma nova técnica de pigmentação manual de
+            sobrancelhas. Essa técnica pode ser utilizada sozinha ou em conjunto
+            de microblading para atingir um resultado ainda mais natural e
+            preenchido.
+          </p>
+
+          <div
+            className="w-[70%] h-[15.5rem] mt-8 m-auto rounded-sm 
           bg-cover bg-no-repeat"
-          style={{ backgroundImage: `url(${microblading})` }}
-        ></div>
+            style={{ backgroundImage: `url(${microshading})` }}
+          ></div>
+        </div>
       </div>
     </div>
   );
